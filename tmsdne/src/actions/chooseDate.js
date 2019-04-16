cfcbf9fd3c27107b32f1a47c158e55384a09687f
dpayton23 <darien.payton71@gmail.com>
2019-04-16 16:41:26 -0500

    action for selecting date

---
+++ tmsdne/src/actions/chooseDate.js
@@ -0,0 +1,14 @@
+export const DATE_START = 'DATE_START';
+export const DATE_SUCCESS = 'DATE_SUCCESS';
+export const DATE_FAILURE = 'DATE_FAILURE';
+
+export const chooseDate = cred => dispatch => {
+    dispatch({type: DATE_START})
+    axios
+    .get()
+    .then(res => {
+        console.log(res)
+        dispatch({type: DATE_SUCCESS, payload: res})
+    })
+    .catch(err => console.log(err))
+}
