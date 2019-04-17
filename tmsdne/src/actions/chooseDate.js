b7fc48fe040179f936feb527d1b006b8d902c147
dpayton23 <darien.payton71@gmail.com>
2019-04-17 09:35:50 -0500

    added url to post request

--- tmsdne/src/actions/chooseDate.js
+++ tmsdne/src/actions/chooseDate.js
@@ -4,10 +4,10 @@ export const DATE_START = 'DATE_START';
 export const DATE_SUCCESS = 'DATE_SUCCESS';
 export const DATE_FAILURE = 'DATE_FAILURE';
 
-export const chooseDate = cred => dispatch => {
+export const chooseDate = data => dispatch => {
     dispatch({type: DATE_START})
     axios
-    .get()
+    .post(`http://834cfcf9.ngrok.io/`, data)
     .then(res => {
         console.log(res)
         dispatch({type: DATE_SUCCESS, payload: res})
