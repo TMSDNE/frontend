0626ca3b2260a71a3b302a23a33dc6856e3ffb88
dpayton23 <darien.payton71@gmail.com>
2019-04-17 10:45:00 -0500

    added redux dependencies and fixed reducer

--- tmsdne/src/index.js
+++ tmsdne/src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-import rootReducer from './reducer';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
+import {reducer} from './reducers';
 
-const store = createStore(rootReducer, applyMiddleware(thunk, logger))
+const store = createStore(reducer, applyMiddleware(thunk, logger))
 
 ReactDOM.render(
     <Provider store={store}>
