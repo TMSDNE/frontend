bd55d3f768ade4062e64cc5e950c4be9b2f15121
dpayton23 <darien.payton71@gmail.com>
2019-04-18 17:24:13 -0500

    updated ArticleList

--- tmsdne/src/components/ArticleList.js
+++ tmsdne/src/components/ArticleList.js
@@ -3,19 +3,37 @@ import Article from './Article'
 import {connect} from 'react-redux';
 import {getData} from '../actions/fetchArticles';
 
-const ArticleList = props => {
-    return (
-        <div>
-            {/* {props.articles.map(article => (
-                <Article article={article} />
-            ))} */}
-        </div>
-    )
+class ArticleList extends React.Component {
+    state = {
+        liked: false 
+    }
+
+    componentDidMount(){
+        this.props.getData(console.log)
+    }
+    
+    handleLike = like => {
+        if (this.state.liked === true) {
+            this.setState({liked: !this.state.liked})
+        } else {
+            this.setState({liked: !this.state.liked})
+        }
+    }
+    
+    render(){
+        return (
+            <div>
+               <p>{this.props.article.commentary}</p>
+               {/* <i className="far fa-heart" onClick={ () => this.handleLike(this.state.liked)}></i> */}
+               
+            </div>
+        )
+    }
 }
 
 const mapStateToProps = state => {
     return {
-        articles: state.article
+        article: state.article
     }
 }
 
