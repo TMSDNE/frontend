4401d152a5e3dd35f0f5ab0da35932910882acda
dpayton23 <darien.payton71@gmail.com>
2019-04-18 14:47:30 -0500

    import date component

--- tmsdne/src/App.js
+++ tmsdne/src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import ArticleList from './components/ArticleList';
 import Header from './components/Header';
+import SelectDate from './components/SelectDate';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Header />
+        <SelectDate />
         <ArticleList />
       </div>
     );
