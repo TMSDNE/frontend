486b967ff9774d6f493c356300a61e7289e8ccca
dpayton23 <darien.payton71@gmail.com>
2019-04-18 17:25:24 -0500

    updated action

--- tmsdne/src/actions/fetchArticles.js
+++ tmsdne/src/actions/fetchArticles.js
@@ -1,16 +1,17 @@
 import axios from 'axios';
 
-export const FETCH_ARTICLE_START = 'FETCH_ARTICLE_START';
-export const FETCH_ARTICLE_SUCCESS = 'FETCH_ARTICLE_SUCCESS';
-export const FETCH_ARTICLE_FAILURE = 'FETCH_ARTICLE_FAILURE';
+const FETCH_ARTICLE_START = 'FETCH_ARTICLE_START';
+const FETCH_ARTICLE_SUCCESS = 'FETCH_ARTICLE_SUCCESS';
+const FETCH_ARTICLE_FAILURE = 'FETCH_ARTICLE_FAILURE';
 
 export const getData = dispatch => {
     dispatch({type: FETCH_ARTICLE_START})
+    
     axios
-    .get(`https://tmsdne.herokuapp.com/api/articles`)
+    .post(`https://tmsdne.herokuapp.com/api/articles`)
     .then(res => {
-        console.log(res)
-        dispatch({type: FETCH_ARTICLE_SUCCESS, payload: res})
+        console.log(res.data)
+        dispatch({type: FETCH_ARTICLE_SUCCESS, payload: res.data})
     })
     .catch(err => console.log(err))
 }
