44e92654b108184a3a0209f72709d7c81eca76b0
dpayton23 <darien.payton71@gmail.com>
2019-04-18 14:46:57 -0500

    removed unnecessary action types

--- tmsdne/src/reducers/index.js
+++ tmsdne/src/reducers/index.js
@@ -1,51 +1,21 @@
 import {
-    LOAD_START,
-    LOAD_SUCCESS,
-    LOAD_FAILURE,
     FETCH_ARTICLE_START,
     FETCH_ARTICLE_SUCCESS,
     FETCH_ARTICLE_FAILURE
 } from '../actions/index';
 
 const initialState = {
-    title: '',
-    category: '',
-    text: '',
-    img: '',
+    article: {},
     fetchingData: false,
     error: ''
 }
 
 export const reducer = (state = initialState, {type, payload}) => {
     switch(type) {
-        case LOAD_START: 
-            return state
-        case LOAD_SUCCESS: 
-            return { 
-                ...state,
-                title: payload,
-                category: payload,
-                text: payload,
-                img: payload
-            }
-        case LOAD_FAILURE:
-            console.log(payload)
-            return {
-                ...state,
-                error: payload.error
-            }
-        case FETCH_ARTICLE_START: 
-            return {
-                ...state,
-                fetchingData: true
-            }
         case FETCH_ARTICLE_SUCCESS:
             return {
                 ...state,
-                title: payload,
-                category: payload,
-                text: payload,
-                img: payload
+                article: payload
             }
         case FETCH_ARTICLE_FAILURE:
             return {
