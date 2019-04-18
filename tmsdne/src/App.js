7c44610da165fb8222e4897d5d41b37c6ffe118d
dpayton23 <darien.payton71@gmail.com>
2019-04-17 19:29:01 -0500

    import ArticleList

--- tmsdne/src/App.js
+++ tmsdne/src/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
+import ArticleList from './components/ArticleList';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <ArticleList />
       </div>
     );
   }
