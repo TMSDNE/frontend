5143252d0280615e623debbd4ee7964cc507cc7e
dpayton23 <darien.payton71@gmail.com>
2019-04-17 20:10:26 -0500

    imported header

--- tmsdne/src/App.js
+++ tmsdne/src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import ArticleList from './components/ArticleList';
+import Header from './components/Header';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Header />
         <ArticleList />
       </div>
     );
