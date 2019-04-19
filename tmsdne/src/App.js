dd12fea3bddea581145e5431c7a65e5264aadb23
dpayton23 <darien.payton71@gmail.com>
2019-04-18 21:50:46 -0500

    added routes

--- tmsdne/src/App.js
+++ tmsdne/src/App.js
@@ -3,16 +3,17 @@ import ArticleList from './components/ArticleList';
 import Header from './components/Header';
 import SelectDate from './components/SelectDate';
 import Login from './components/Login';
+import {PrivateRoute} from './components/PrivateRoute';
+import {Route} from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-
+        <Route path='/login' component={Login} />
         { /* <Header /> */ }
-        <Login login={} />
         {/* <SelectDate /> */}
-        { /*<ArticleList /> */ }
+        <PrivateRoute exact path='/' component={ArticleList} />
       </div>
     );
   }
--- tmsdne/src/index.js
+++ tmsdne/src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {BrowserRouter as Router} from 'react-router-dom';
 import App from './App';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
@@ -11,6 +12,8 @@ const store = createStore(reducer, applyMiddleware(thunk, logger))
 
 ReactDOM.render(
     <Provider store={store}>
-        <App />
+        <Router>
+            <App />
+        </Router>
     </Provider>, 
     document.getElementById('root'));
